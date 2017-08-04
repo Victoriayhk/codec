e45ab6a289775e2798079f1efcf124831624e595
李春尧 <lichunyao@zhbang.com>
2017-08-05 01:06:11 +0800

    pa

--- codec/Pattern.cpp
+++ codec/Pattern.cpp
@@ -8,9 +8,117 @@
 //	return pattern_number; 
 //}
 
-void Pattern::pattern(Block&,ResidualBlock &,int,int,int,int,BlockBufferPool&,int pattern_type,AVFormat &para){
+#define IN_AREA(i, j, h, w) (0 <= i && i < h && 0 <= j && j <= w) 
 
+double Pattern::predict(Block& block,ResidualBlock & r_block,int start_r,int start_c,int end_r,int end_c, BlockBufferPool& whole_frame,int pattern_type, AVFormat &para){
+	int block_w, block_h;
+	r_block.getBlockSize(para, block_w, block_h);							// 当前block的宽和高
+	int i_offset = (r_block.block_id / para.block_num_per_row) * block_h;	// 当前block的起始像素所在Frame的行
+	int j_offset = (r_block.block_id % para.block_num_per_row) * block_w;	// 当前block的起始像素所在Frame的列
+
+	double diff = 0;
+	if (pattern_type == 0) {		// 竖向预测, 参考小块上方的一条像素
+		if (i_offset > 0) {			// 本帧内, 上方有可参考的像素
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					r_block.data[i * block_w + j] = (int16_t)block.data[i * block_w + j] - whole_frame.getValue(i_offset - 1, j_offset + j);
+					diff += r_block.data[i * block_w + j] * r_block.data[i * block_w + j];
+				}
+			}
+		} else {					// 本帧内, 上方有可参考的像素, 与128求差
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					r_block.data[i * block_w + j] = (int16_t)block.data[i * block_w + j] - 128;
+					diff += r_block.data[i * block_w + j] * r_block.data[i * block_w + j];
+				}
+			}
+		}
+	} else if (pattern_type == 1) {	// 横向向预测, 参考小块左方的一条像素
+		if (j_offset > 0) {			// 本帧内, 左方有可参考的像素
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					r_block.data[i * block_w + j] = (int16_t)block.data[i * block_w + j] - whole_frame.getValue(i_offset + i, j_offset - 1);
+					diff += r_block.data[i * block_w + j] * r_block.data[i * block_w + j];
+				}
+			}
+		} else {					// 本帧内, 左方有可参考的像素, 与128求差
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					r_block.data[i * block_w + j] = (int16_t)block.data[i * block_w + j] - 128;
+					diff += r_block.data[i * block_w + j] * r_block.data[i * block_w + j];
+				}
+			}
+		}
+	} else if (pattern_type == 2) {	// 左上方方向预测
+		for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+			for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+				int c_i = max(i_offset - 1, i - (j - j_offset));
+				int c_j = max(j_offset - 1, j - (i - i_offset));
+				if (c_i > 0 && c_j > 0) {
+					r_block.data[i * block_w + j] = (int16_t)block.data[i * block_w + j] - whole_frame.getValue(c_i, c_j);
+				} else {	
+					r_block.data[i * block_w + j] = (int16_t)block.data[i * block_w + j] - 128;
+				}
+				diff += r_block.data[i * block_w + j] * r_block.data[i * block_w + j];
+			}
+		}
+	}
+
+	return diff;
 }
 
-void Pattern::re_pattern(Block&,ResidualBlock &,int,int,int,int,BlockBufferPool&,int pattern_type,AVFormat &para){
+void Pattern::de_predict(Block& blk,ResidualBlock & r_block,int start_r,int start_c,int end_r,int end_c, BlockBufferPool& b_pool,int pattern_type, AVFormat &para) {
+	int block_w, block_h;
+	r_block.getBlockSize(para, block_w, block_h);							// 当前block的宽和高
+	int i_offset = (r_block.block_id / para.block_num_per_row) * block_h;	// 当前block的起始像素所在Frame的行
+	int j_offset = (r_block.block_id % para.block_num_per_row) * block_w;	// 当前block的起始像素所在Frame的列
+
+	if (pattern_type == 0) {		// 竖向预测, 参考小块上方的一条像素
+		if (i_offset > 0) {			// 本帧内, 上方有可参考的像素
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					blk.data[i * block_w + j] = r_block.data[i * block_w + j] + b_pool.getValue(i_offset - 1, j_offset + j);
+					b_pool.setValue(i_offset + i, j_offset +j, r_block.data[i * block_w + j] + b_pool.getValue(i_offset - 1, j_offset + j));
+				}
+			}
+		} else {					// 本帧内, 上方有可参考的像素, 与128求差
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					blk.data[i * block_w + j] = r_block.data[i * block_w + j] + 128;
+					b_pool.setValue(i_offset + i, j_offset +j, r_block.data[i * block_w + j] + 128);
+				}
+			}
+		}
+	} else if (pattern_type == 1) {	// 横向向预测, 参考小块左方的一条像素
+		if (j_offset > 0) {			// 本帧内, 左方有可参考的像素
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					blk.data[i * block_w + j] = r_block.data[i * block_w + j] + b_pool.getValue(i_offset + i, j_offset - 1);
+					b_pool.setValue(i_offset + i, j_offset +j, (int16_t)r_block.data[i * block_w + j] + b_pool.getValue(i_offset + i, j_offset - 1));
+				}
+			}
+		} else {					// 本帧内, 左方有可参考的像素, 与128求差
+			for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+					blk.data[i * block_w + j] = r_block.data[i * block_w + j] + 128;
+					b_pool.setValue(i_offset + i, j_offset +j, (int16_t)r_block.data[i * block_w + j] + 128);
+				}
+			}
+		}
+	} else if (pattern_type == 2) {	// 左上方方向预测
+		for (int i = start_r; i <= end_r && i + i_offset < para.height; i++) {
+				for (int j = start_c; j <= end_c && j + j_offset < para.width; j++) {
+				int c_i = max(i_offset - 1, i - (j - j_offset));
+				int c_j = max(j_offset - 1, j - (i - i_offset));
+				
+				if (c_i > 0 && c_j > 0) {
+					blk.data[i * block_w + j] = r_block.data[i * block_w + j] + b_pool.getValue(c_i, c_j);
+					b_pool.setValue(i_offset + i, j_offset +j, (int16_t)r_block.data[i * block_w + j] + b_pool.getValue(c_i, c_j));
+				} else {	
+					blk.data[i * block_w + j] = r_block.data[i * block_w + j] + 128;
+					b_pool.setValue(i_offset + i, j_offset +j, (int16_t)r_block.data[i * block_w + j] + 128);
+				}
+			}
+		}
+	}
 }
--- codec/predict.cpp
+++ codec/predict.cpp
@@ -20,12 +20,12 @@ int predict_block_intra(Block &block, ResidualBlock  &mode_result, FrameBufferPo
 	score = 0;
 	return 0;
 }
-int predict(Block &block,ResidualBlock  &residual_block,Tree &tree,BlockBufferPool & block_buffer_pool,FrameBufferPool &frame_pool,Block & buffer_block,AVFormat & para,double & min_score)
+int predict_(Block &block,ResidualBlock  &residual_block,Tree &tree,BlockBufferPool & block_buffer_pool,FrameBufferPool &frame_pool,Block & buffer_block,AVFormat & para,double & min_score)
 {
 
 	int tph = tree.left_top_h,tpw = tree.left_top_w,brh = tree.right_bottom_h, brw = tree.right_bottom_w;
 	if(tree.data->pre_type == Node::INTRA_PREDICTION){
-		Pattern::pattern(block,residual_block,tph,tpw,brh,brw,block_buffer_pool,tree.data->prediction,para);
+		Pattern::predict(block,residual_block,tph,tpw,brh,brw,block_buffer_pool,tree.data->prediction,para);
 	}else{
 	}
 
@@ -41,13 +41,13 @@ double intra_predict(Block &block,ResidualBlock  &residual_block,Tree &tree,Bloc
 	int tph = tree.left_top_h,tpw = tree.left_top_w,brh = tree.right_bottom_h, brw = tree.right_bottom_w;
 	double score = min_score;
 	for(int i = 0; i < pattern_num; ++i){
-		Pattern::pattern(block,residual_block,tph,tpw,brh,brw,block_buffer_pool,i,para);
+		Pattern::predict(block,residual_block,tph,tpw,brh,brw,block_buffer_pool,i,para);
 
 		quantization(tph,tpw,brh,brw, residual_block , para);
 
 		Reverse_quantization(tph,tpw,brh,brw , residual_block , para);
 
-		Pattern::re_pattern(buffer_block,residual_block,tph,tpw,brh,brw ,block_buffer_pool,i,para);
+		Pattern::de_predict(buffer_block,residual_block,tph,tpw,brh,brw ,block_buffer_pool,i,para);
 		
 		score = calc_coef(tph,tpw,brh,brw,buffer_block,block);
 		if(score < min_score){
