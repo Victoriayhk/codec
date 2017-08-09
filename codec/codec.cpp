30248527b2d63578e32ddd59f7517c42b6f3feb1
dcc <dongchenchenhaohao@163.com>
2017-08-09 11:20:24 +0800

    DEBUG and RELEASE

--- codec/codec.cpp
+++ codec/codec.cpp
@@ -18,19 +18,22 @@
 
 //#include "huffman.h"
 
+//#define DEBUG
 int main(int argc, char * argv[])
 {
-	int start_time=clock();
+	int proc_start,proc_end;
+	int start_time,end_time;
+	proc_start=clock();
 	AVFormat para;
 	para.load(argc,argv);
-	//para.quantizationY=10;
-	//para.quantizationU=15;
-	//para.quantizationV=15;
-	//para.frame_num=50;
+
+#ifdef DEBUG
+	para.quantizationY=10;
+    para.quantizationU=15;
+	para.quantizationV=15;
+	para.frame_num=1;
+#endif
 	Frame frame;
-	
-	
-	//unsigned char * buff = new unsigned char[1000000000];
 	Frame frame1;
 	PKT pkt;
 	PKT pkt1;
@@ -42,74 +45,72 @@ int main(int argc, char * argv[])
 	frame_pool[0] = new FrameBufferPool(10,BlockBufferPool(para.height,para.width));
 	frame_pool[1] = new FrameBufferPool(10,BlockBufferPool(para.height/2,para.width/2));
 	frame_pool[2] = new FrameBufferPool(10,BlockBufferPool(para.height/2,para.width/2));
-
-
 	para.video = fopen(para.file_name,"rb");
 	para.out_video = fopen(para.out_file_name,"wb");	
-
-	para.frame_num = 1;
 	for(int i = 0; i < para.frame_num; ++i){
+	#ifdef DEBUG
+		start_time=clock();
+	#endif
 
-		int start_time=clock();
-		yuv_read(para,frame);
-		
+		yuv_read(para,frame);	
+
+	#ifdef DEBUG
+		end_time=clock();
+		std::cout<<" yuv readtime is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;			
+	#endif
+	#ifdef DEBUG
+		start_time=clock();
+	#endif
+
 		int errno1 = encode(frame,para,pkt,frame_pool);
 
-		
-		//pkt.stream_write(para);
-		//int end_time=clock();
-
-		int start_time_write=clock();
+	#ifdef DEBUG
+		end_time=clock();			
+		std::cout<<"encode Frame "<<i<<endl<< " encode time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<endl;
+	#endif
+	#ifdef DEBUG
+		start_time=clock();
+	#endif
 		pkt.stream_write(para);
-//		int end_time_write=clock();
-		//std::cout<<" Stream wirte Running time is: "<<static_cast<double>(end_time_write-start_time_write)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;	
-		
-		int end_time=clock();
-		std::cout<<"decode Frame "<<i<<endl<< " Running time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<endl<<"wirte Running time is:"<<static_cast<double>(end_time_write-start_time_write)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;	
-
-		
+	#ifdef DEBUG
+		end_time=clock();
+		std::cout<<"pkt write time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;			
 		for(auto i =0; i != 8;++i){
 			printf("%d ",pkt.Ylist[0].data[i]);
 		}
 		printf("\n");
-
+	#endif
 	}
-//	printf("=================================================================\n");
+#ifdef DEBUG
+	printf("=================================================================\n");
+#endif
 	fclose(para.stream_writer);
 		para.stream_writer = nullptr;
 	for(int i = 0; i < para.frame_num; ++i){
-//		int start_time=clock();
-		pkt1.stream_read(para);		
+	#ifdef DEBUG
+		start_time=clock();
+	#endif
+		pkt1.stream_read(para);	
+		printf("decode Frame %d\n",i);
 		int errno2 = decode(frame1,para,pkt1,frame_pool);
-
-//		int start_time_read=clock();
-		pkt1.stream_read(para);
-//		int end_time_read=clock();
-		//std::cout<< "Read Running time is: "<<static_cast<double>(end_time_read-start_time_read)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;	
-		int errno2 = decode(frame1,para,pkt1,frame_pool);
+	#ifdef DEBUG
+		end_time=clock();			
+		std::cout<<"decode Frame "<<i<<endl<< " decode and pkt read time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<endl;
+	#endif
 		int result = yuv_write(para, frame1);
-//		int end_time=clock();
-//		printf("decode Frame %d\n",i);
-//		std::cout<<i<< "Running time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;	
-//		std::cout<< "Read Running time is: "<<static_cast<double>(end_time_read-start_time_read)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;
-		//printf("decode Frame %d\n",i);
-	}
-
-	
+}
 
 	fclose(para.video);	
-
 	fclose(para.out_video);	
-
 	fclose(para.stream_reader);
 	para.stream_reader = nullptr;
-
-
 	for(int i = 0; i < 3; ++i)
 		delete frame_pool[i];
-
-	int end_time=clock();
-	std::cout<< "process running time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;
+	proc_end=clock();
+	std::cout<< "process running time is: "<<static_cast<double>(proc_end-proc_start)/CLOCKS_PER_SEC*1000<<"ms"<<std::endl;
+	#ifdef DEBUG
+		system("pause");
+	#endif
 	return 0;
 
 }
